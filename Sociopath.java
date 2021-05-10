d067902a363ef272e160e025a403ec61bf099420
Amir Firdaus Bin Abdul Hadi <80340482+amirFirdaus39@users.noreply.github.com>
2021-05-10 10:45:10 +0800

    Add files via upload

---
+++ Sociopath.java
@@ -0,0 +1,34 @@
+public class Sociopath {
+    public static void main(String[] args) {
+        
+    }
+    
+    public void Event1(){
+        //code for event 1
+    }
+    
+    public void Event2(){
+        //code for event 2
+
+    }
+    
+    public void Event3(){
+        //code for event 3
+
+    }
+    
+    public void Event4(){
+        //code for event 4
+
+    }
+    
+    public void Event5(){
+        //code for event 5
+
+    }
+    
+    public void Event6(){
+        //code for event 6
+
+    }
+}
